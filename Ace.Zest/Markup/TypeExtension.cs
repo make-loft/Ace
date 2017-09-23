8e45da078a7293c9ff388cd50019c739cf6aec60
makeman <makeman@tut.by>
2017-09-23 21:14:52 +0300

    MAKE !

---
+++ Ace.Zest/Markup/TypeExtension.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace Ace.Markup
+{
+    public class TypeExtension : Patterns.AMarkupExtension
+    {
+        [TypeConverter(typeof(TypeTypeConverter))]
+        public Type Key { get; set; }
+
+        public override object Provide(object targetObject, object targetProperty = null) => Key;
+    }
+}
