5e53acfbdf659cd3426cc0863697354bd147d8ec
makeman <makeman@tut.by>
2017-10-31 19:39:01 +0300

    MAKE !

--- Ace.Core/SmartSet.cs
+++ Ace.Core/SmartSet.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Collections.Specialized;
 using System.Runtime.Serialization;
+using Args = System.Collections.Specialized.NotifyCollectionChangedEventArgs;
 
 namespace Ace
 {
@@ -18,15 +19,15 @@ namespace Ace
         public void Initialize(StreamingContext context = default(StreamingContext))
         {
             Source = Source ?? new List<T>();
-	        CollectionChanged = null;
+            CollectionChanged = null;
             CollectionChanged += (sender, args) => RaisePropertyChanged("Count");
         }
 
         public SmartSet(IEnumerable<T> collection)
         {
             Source = new List<T>(collection);
-	        CollectionChanged = null;
-			CollectionChanged += (sender, args) => RaisePropertyChanged("Count");
+            CollectionChanged = null;
+            CollectionChanged += (sender, args) => RaisePropertyChanged("Count");
         }
 
         public IEnumerator<T> GetEnumerator() => Source.GetEnumerator();
@@ -37,46 +38,46 @@ namespace Ace
         {
             Source.Add(item);
             CollectionChanged(this,
-                new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Add, item, Source.Count - 1));
+                new Args(NotifyCollectionChangedAction.Add, item, Source.Count - 1));
         }
 
         public int Add(object value)
         {
-            Source.Add((T)value);
+            Source.Add((T) value);
             CollectionChanged(this,
-                new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Add, value, Source.Count - 1));
+                new Args(NotifyCollectionChangedAction.Add, value, Source.Count - 1));
             return 1;
         }
 
-	    public void AddRange(IEnumerable<T> collection)
-	    {
-		    Source.AddRange(collection);
-		    CollectionChanged(this, new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Reset));
-		}
-
-		void IList.Clear()
+        public void AddRange(IEnumerable<T> collection)
         {
-            Source.Clear();
-            CollectionChanged(this, new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Reset));
+            Source.AddRange(collection);
+            CollectionChanged(this, new Args(NotifyCollectionChangedAction.Reset));
         }
 
-	    public bool Contains(object value) => Source.Contains((T) value);
+        void IList.Clear()
+        {
+            Source.Clear();
+            CollectionChanged(this, new Args(NotifyCollectionChangedAction.Reset));
+        }
 
-	    public int IndexOf(object value) => Source.IndexOf((T) value);
+        public bool Contains(object value) => Source.Contains((T) value);
+
+        public int IndexOf(object value) => Source.IndexOf((T) value);
 
         public void Insert(int index, object value)
         {
-            Source.Insert(index, (T)value);
+            Source.Insert(index, (T) value);
             CollectionChanged(this,
-                new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Add, value, Source.Count - 1));
+                new Args(NotifyCollectionChangedAction.Add, value, Source.Count - 1));
         }
 
         public void Remove(object value)
         {
-            var index = Source.IndexOf((T)value);
-            Source.Remove((T)value);
+            var index = Source.IndexOf((T) value);
+            Source.Remove((T) value);
             CollectionChanged(this,
-                new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Remove, value, index));
+                new Args(NotifyCollectionChangedAction.Remove, value, index));
         }
 
         void IList.RemoveAt(int index)
@@ -84,18 +85,18 @@ namespace Ace
             var value = Source[index];
             Source.RemoveAt(index);
             CollectionChanged(this,
-                new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Remove, value, index));
+                new Args(NotifyCollectionChangedAction.Remove, value, index));
         }
 
         public void Clear()
         {
             Source.Clear();
-            CollectionChanged(this, new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Reset));
+            CollectionChanged(this, new Args(NotifyCollectionChangedAction.Reset));
         }
 
-        public bool IsFixedSize => ((IList)Source).IsFixedSize;
+        public bool IsFixedSize => ((IList) Source).IsFixedSize;
 
-        bool IList.IsReadOnly => ((IList)Source).IsReadOnly;
+        bool IList.IsReadOnly => ((IList) Source).IsReadOnly;
 
         public T this[int index]
         {
@@ -109,16 +110,15 @@ namespace Ace
             set
             {
                 var oldValue = Source[index];
-	            Source[index] = (T) value;
-                CollectionChanged(this,
-                    new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Replace, value, oldValue, index));
+                Source[index] = (T) value;
+                CollectionChanged(this, new Args(NotifyCollectionChangedAction.Replace, value, oldValue, index));
             }
         }
 
         void ICollection<T>.Clear()
         {
             Source.Clear();
-            CollectionChanged(this, new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Reset));
+            CollectionChanged(this, new Args(NotifyCollectionChangedAction.Reset));
         }
 
         public bool Contains(T item) => Source.Contains(item);
@@ -129,38 +129,35 @@ namespace Ace
         {
             var index = Source.IndexOf(item);
             var result = Source.Remove(item);
-            CollectionChanged(this,
-                new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Remove, item, index));
+            CollectionChanged(this, new Args(NotifyCollectionChangedAction.Remove, item, index));
             return result;
         }
 
-        public void CopyTo(Array array, int index) => ((ICollection)Source).CopyTo(array, index);
+        public void CopyTo(Array array, int index) => ((ICollection) Source).CopyTo(array, index);
 
         int ICollection.Count => Source.Count;
 
-	    public bool IsSynchronized => ((ICollection) Source).IsSynchronized;
+        public bool IsSynchronized => ((ICollection) Source).IsSynchronized;
 
-	    public object SyncRoot => ((ICollection) Source).SyncRoot;
+        public object SyncRoot => ((ICollection) Source).SyncRoot;
 
         int ICollection<T>.Count => Source.Count;
 
-        bool ICollection<T>.IsReadOnly => ((ICollection<T>)Source).IsReadOnly;
+        bool ICollection<T>.IsReadOnly => ((ICollection<T>) Source).IsReadOnly;
 
         public int IndexOf(T item) => Source.IndexOf(item);
 
         public void Insert(int index, T item)
         {
             Source.Insert(index, item);
-            CollectionChanged(this,
-                new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Add, item, index));
+            CollectionChanged(this, new Args(NotifyCollectionChangedAction.Add, item, index));
         }
 
         void IList<T>.RemoveAt(int index)
         {
             var item = Source[index];
             Source.RemoveAt(index);
-            CollectionChanged(this,
-                new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Remove, item, index));
+            CollectionChanged(this, new Args(NotifyCollectionChangedAction.Remove, item, index));
         }
 
         T IList<T>.this[int index]
@@ -169,11 +166,14 @@ namespace Ace
             set
             {
                 Source[index] = value;
-                CollectionChanged(this, new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Replace));
+                CollectionChanged(this, new Args(NotifyCollectionChangedAction.Replace));
             }
         }
 
         public int Count => Source.Count;
+
+        public void RaiseCollectionChanged() => CollectionChanged(this, new Args(NotifyCollectionChangedAction.Reset));
+        public void RaiseCollectionChanged(object sender, Args args) => CollectionChanged(sender, args);
 
         public event NotifyCollectionChangedEventHandler CollectionChanged = (sender, args) => { };
     }
