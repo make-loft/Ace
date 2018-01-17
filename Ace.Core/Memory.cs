74bc0c62c1d0f6e83d668c7b8f6c108aaca3e60e
makeman <makeman@tut.by>
2018-01-17 22:29:13 +0300

    MAKE !

--- Ace.Core/Memory.cs
+++ Ace.Core/Memory.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Diagnostics;
 using System.IO;
 using System.Runtime.Serialization;
 using System.Text;
@@ -17,9 +16,10 @@ namespace Ace
 		public string KeyFormat { get; }
 
 		public KeepProfile KeepProfile = new KeepProfile {MapPairSplitter = "\t", Delimiter = ""};
+
 		public ReplicationProfile ReplicationProfile =
 			new ReplicationProfile {SimplifyMaps = true, SimplifySets = true};
-		
+
 		public Memory(IStorage storage, string keyFormat = "{0}.json")
 		{
 			Storage = storage;
@@ -30,54 +30,54 @@ namespace Ace
 			Attribute.IsDefined(type, typeof(DataContractAttribute)) ||
 			Attribute.IsDefined(type, typeof(CollectionDataContractAttribute));
 
-		protected static object Activate(Type type, params object[] constructorArgs) =>
-			Activator.CreateInstance(type, constructorArgs);
-
-		public object Revive(string key, Type type, params object[] constructorArgs)
-		{
-			try
-			{
-				return HasDataContract(type) ? Decode(key, type) : Activate(type, constructorArgs);
-			}
-			catch (Exception exception)
-			{
-				Debug.WriteLine(exception.ToString());
-				return Activate(type, constructorArgs);
-			}
-		}
+		public object Revive(string key, Type type, params object[] constructorArgs) =>
+			(HasDataContract(type) ? Decode(key, type) : null)
+			?? ActivationRequired?.Invoke(key, type, constructorArgs)
+			?? Activator.CreateInstance(type, constructorArgs);
 
 		public void Keep<TValue>(TValue item, string key = null)
 		{
-			try
-			{
-				if (HasDataContract(item.GetType())) Encode(item, key);
-			}
-			catch (Exception exception)
-			{
-				Debug.WriteLine(exception.ToString());
-			}
+			if (HasDataContract(item.GetType())) Encode(item, key);
 		}
 
 		private object Decode(string key, Type type)
 		{
-			var storageKey = MakeStorageKey(key, type);
-			//var data = File.ReadAllText(storageKey);
-			using (var stream = Storage.GetReadStream(storageKey))
-			using (var streamReader = new StreamReader(stream, Encoding.UTF8))
-				return streamReader.ReadToEnd().ParseSnapshot(ReplicationProfile, KeepProfile).ReplicateGraph(type);
+			try
+			{
+				var storageKey = MakeStorageKey(key, type);
+				//var data = File.ReadAllText(storageKey);
+				using (var stream = Storage.GetReadStream(storageKey))
+				using (var streamReader = new StreamReader(stream, Encoding.UTF8))
+					return streamReader.ReadToEnd().ParseSnapshot(ReplicationProfile, KeepProfile).ReplicateGraph(type);
+			}
+			catch (Exception exception)
+			{
+				DecodeFailed?.Invoke(key, type, exception);
+				return null;
+			}
 		}
 
 		private void Encode(object item, string key)
 		{
-			var snapshot = item.CreateSnapshot(ReplicationProfile);
-			var data = snapshot.ToString(KeepProfile);
-			var storageKey = MakeStorageKey(key, item.GetType());
-			//File.WriteAllText(storageKey, data);
-			using (var stream = Storage.GetWriteStream(storageKey))
-			using (var streamWriter = new StreamWriter(stream, Encoding.UTF8))
-				streamWriter.Write(data);
+			try
+			{
+				var snapshot = item.CreateSnapshot(ReplicationProfile);
+				var data = snapshot.ToString(KeepProfile);
+				var storageKey = MakeStorageKey(key, item.GetType());
+				//File.WriteAllText(storageKey, data);
+				using (var stream = Storage.GetWriteStream(storageKey))
+				using (var streamWriter = new StreamWriter(stream, Encoding.UTF8))
+					streamWriter.Write(data);
+			}
+			catch (Exception exception)
+			{
+				EncodeFailed?.Invoke(key, item, exception);
+			}
 		}
 
+		public event ActivationQueryHandler ActivationRequired;
+		public event DecodeFailedHandler DecodeFailed;
+		public event EncodeFailedHandler EncodeFailed;
 		public bool Check<TValue>(string key = null) => Storage.HasKey(MakeStorageKey(key, typeof(TValue)));
 		public void Destroy<TValue>(string key = null) => Storage.DeleteKey(MakeStorageKey(key, typeof(TValue)));
 		private string MakeStorageKey(string key, Type type) => string.Format(KeyFormat ?? "{0}", key ?? type.Name);
--- Ace.Core/Patterns/IMemoryBox.cs
+++ Ace.Core/Patterns/IMemoryBox.cs
@@ -2,8 +2,18 @@
 
 namespace Ace.Patterns
 {
+	public delegate object ActivationQueryHandler(string key, Type type, params object[] constructorArgs);
+
+	public delegate void DecodeFailedHandler(string key, Type type, Exception exception);
+	
+	public delegate void EncodeFailedHandler(string key, object item, Exception exception);
+	
 	public interface IMemoryBox
 	{
+		event ActivationQueryHandler ActivationRequired;
+		event DecodeFailedHandler DecodeFailed;
+		event EncodeFailedHandler EncodeFailed;
+		
 		bool Check<TItem>(string key = null);
 		void Destroy<TItem>(string key = null);
 		void Keep<TItem>(TItem item, string key = null);
