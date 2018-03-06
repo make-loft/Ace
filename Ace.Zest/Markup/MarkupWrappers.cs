d98aa4d622c15a50a80109ba25e1653006f17fd2
makeman <makeman@tut.by>
2018-03-06 14:11:55 +0300

    MAKE !

---
+++ Ace.Zest/Markup/MarkupWrappers.cs
@@ -0,0 +1,17 @@
+﻿using Ace.Markup.Patterns;
+
+namespace Ace.Markup
+{
+	public class MarkupWrapper<T> : AMarkupExtension
+	{
+		public T Value { get; set; }
+
+		public override object Provide(object targetObject, object targetProperty = null) => Value;
+	}
+
+#if XAMARIN
+	public class GridLength : MarkupWrapper<Xamarin.Forms.GridLength> { }
+	public class LayoutOptions : MarkupWrapper<Xamarin.Forms.LayoutOptions> { }
+#else
+#endif
+}
