045d91b1a6c79f23801595c73d1783756fe3b18a
makeman <makeman@tut.by>
2018-04-14 23:47:11 +0300

    MAKE !

--- Ace.Base/Sugar/LE.Deconstruct.cs
+++ Ace.Base/Sugar/LE.Deconstruct.cs
@@ -9,9 +9,9 @@ namespace Ace
         public static void Deconstruct<T>(this T o, out T a, out T b, out T c) => a = b = c = o;
         public static void Deconstruct<T>(this T o, out T a, out T b, out T c, out T d) => a = b = c = d = o;
         public static void Deconstruct<T>(this T o, out T a, out T b, out T c, out T d, out T e) => a = b = c = d = e = o;
-        public static void Deconstruct<T>(this T o, out T a, out T b, out T c, out T d, out T e, out T f) => a =b = c = d = e = f = o;
-        public static void Deconstruct<T>(this T o, out T a, out T b, out T c, out T d, out T e, out T f, out T g) => a =b = c = d = e = f = g = o;
-        public static void Deconstruct<T>(this T o, out T a, out T b, out T c, out T d, out T e, out T f, out T g, out T h) => a =b = c = d = e = f = g = h = o;
+        public static void Deconstruct<T>(this T o, out T a, out T b, out T c, out T d, out T e, out T f) => a = b = c = d = e = f = o;
+        public static void Deconstruct<T>(this T o, out T a, out T b, out T c, out T d, out T e, out T f, out T g) => a = b = c = d = e = f = g = o;
+        public static void Deconstruct<T>(this T o, out T a, out T b, out T c, out T d, out T e, out T f, out T g, out T h) => a = b = c = d = e = f = g = h = o;
         
         //public static void To<T>(this T o, out T a) => a = o;
         public static T To<T>(this T o, out T a, out T b) => a = b = o;
