478328f5ba59c664d01f1cb596e9924f2f4406cd
makeman <makeman@tut.by>
2018-07-27 00:10:03 +0300

    MAKE !

--- Ace.Tests/Ace.Base.Console/Program.cs
+++ Ace.Tests/Ace.Base.Console/Program.cs
@@ -6,23 +6,8 @@ namespace Ace.Base.Console
 {
 	static class Program
 	{
-		class Point
-		{
-			private int X, Y, Z;
-
-			public void Deconstruct(out int x, out int y, out int z)
-			{
-				x = X;
-				y = Y;
-				z = Z;
-			}
-		}
 		static void Main(string[] args)
-		{
-			var p = new Point();
-			var (x, y, z) = p;
-			
-			
+		{			
 			try
 			{
 				var tests = new CoreTests();
