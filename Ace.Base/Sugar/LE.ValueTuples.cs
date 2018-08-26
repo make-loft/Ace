fbef1dd1cb5ee1a97456299c2c649d564837d967
makeman <makeman@tut.by>
2018-08-26 23:30:42 +0300

    MAKE !

---
+++ Ace.Base/Sugar/LE.ValueTuples.cs
@@ -0,0 +1,58 @@
+﻿#if ValueTuples
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using Ace;
+
+namespace Ace.Base.Console
+{
+	public static partial class LE
+	{
+		public static bool IsNot<T>(this T x, params object[] y) where T : ITuple => !x.Is(y);
+
+		public static bool Is<T>(this T x, params object[] y) where T : ITuple
+		{
+			if (x.Length.IsNot(y.Length)) return false;
+			for (var i = 0; i < y.Length; i++)
+			{
+				if (x[i].IsNot(y[i])) return false;
+			}
+
+			return true;
+		}
+
+		//public static bool Is<A>(this (A) x, A a) => x.Is((a));
+		public static bool Is<A, B>(this (A, B) x, A a, B b) => x.Is((a, b));
+		public static bool Is<A, B, C>(this (A, B, C) x, A a, B b, C c) => x.Is((a, b, c));
+		public static bool Is<A, B, C, D>(this (A, B, C, D) x, A a, B b, C c, D d) => x.Is((a, b, c, d));
+		// etc...
+
+
+		//public static bool Is<A>(this (A) x, A a) => !x.Is((a));
+		public static bool IsNot<A, B>(this (A, B) x, A a, B b) => !x.Is((a, b));
+		public static bool IsNot<A, B, C>(this (A, B, C) x, A a, B b, C c) => !x.Is((a, b, c));
+		public static bool IsNot<A, B, C, D>(this (A, B, C, D) x, A a, B b, C c, D d) => !x.Is((a, b, c, d));
+		// etc...
+
+		public static bool IsAny<TTuple>(this object o, TTuple tuple) where TTuple : ITuple => tuple.Contains(o);
+
+		//public static bool IsAny<A>(this object o, A a) => o.Is(a); 
+		public static bool IsAny<A, B>(this object o, A a, B b) => o.Is(a) || o.Is(b);
+		public static bool IsAny<A, B, C>(this object o, A a, B b, C c) => o.IsAny(a, b) || o.Is(c);
+		public static bool IsAny<A, B, C, D>(this object o, A a, B b, C c, D d) => o.IsAny(a, b, c) || o.Is(d);
+		public static bool IsAny<A, B, C, D, E>(this object o, A a, B b, C c, D d, E e) => o.IsAny(a, b, c, d) || o.Is(e);
+		public static bool IsAny<A, B, C, D, E, F>(this object o, A a, B b, C c, D d, E e , F f) => o.IsAny(a, b, c, d, e) || o.Is(f);
+		public static bool IsAny<A, B, C, D, E, F, G>(this object o, A a, B b, C c, D d, E e , F f, G g) => o.IsAny(a, b, c, d, e, f) || o.Is(g);
+		public static bool IsAny<A, B, C, D, E, F, G, H>(this object o, A a, B b, C c, D d, E e , F f, G g, H h) => o.IsAny(a, b, c, d, e, f, g) || o.Is(h);
+
+		public static bool Contains<TTuple>(this TTuple tuple, object o) where TTuple : ITuple
+		{
+			for (var i = 0; i < tuple.Length; i++)
+			{
+				if (tuple[i].Is(o)) return true;
+			}
+
+			return false;
+		}
+	}
+}
+#endif
---
+++ Ace.Tests/Ace.Base.Sandbox/Drafts/Stack.cs
@@ -0,0 +1,89 @@
+﻿using System;
+
+namespace Ace.Base.Sandbox.Drafts
+{
+	public interface ISpan<T>
+	{	
+		int Length { get; }
+
+		T this[int index] { get; set; }
+	}
+	
+	public struct Span<T> : ISpan<T>
+	{
+		public int Length => 0;
+
+		public T this[int index]
+		{
+			get => throw new ArgumentOutOfRangeException();
+			set => throw new ArgumentOutOfRangeException();
+		}
+
+		public override string ToString() { return "()"; }
+	}
+	
+	public struct Span<T, TRest> : ISpan<T>
+		where TRest : struct, ISpan<T>
+	{
+		public const int _spanSize = 8;
+		public T A, B, C, D, E, F, G, H;
+		public TRest Rest;
+		public int Length => _spanSize + Rest.Length;
+
+		public Span(TRest rest)
+		{
+			A = B = C = D = E = F = G = H = default(T);
+			Rest = rest;
+		}
+
+		public T this[int index]
+		{
+			get
+			{
+				var rest = index % Length;
+				if (rest == 0) return A;
+				if (rest == 1) return B;
+				if (rest == 2) return C;
+				if (rest == 3) return D;
+				if (rest == 4) return E;
+				if (rest == 5) return F;
+				if (rest == 6) return G;
+				if (rest == 7) return H;
+				return Rest[index - _spanSize];
+			}
+			set
+			{
+				var rest = index % Length;
+				if (rest == 0) A = value;
+				else if (rest == 1) B = value;
+				else if (rest == 2) C = value;
+				else if (rest == 3) C = value;
+				else if (rest == 4) C = value;
+				else if (rest == 5) C = value;
+				else if (rest == 6) C = value;
+				else if (rest == 7) C = value;
+				else Rest[index - _spanSize] = value;
+			}
+		}
+
+		public override string ToString() => $"({A}, {B}, {C}, {D}, {E}, {F}, {G}, {H}, {Rest})";
+	}
+	
+	public static class Stack
+	{
+		private static Span<T, TRest> Add<T, TRest>(this TRest rest)
+			where TRest : struct, ISpan<T> => new Span<T, TRest>(rest);
+	
+		public static Span<T, Span<T>> Alloc8<T>() =>
+			new Span<T>().Add<T, Span<T>>();
+		
+		public static Span<T, Span<T, Span<T>>> Alloc16<T>() =>
+			Alloc8<T>().Add<T, Span<T, Span<T>>>();
+		
+		public static Span<T, Span<T, Span<T, Span<T>>>> Alloc24<T>() =>
+			Alloc16<T>().Add<T, Span<T, Span<T, Span<T>>>>();
+		
+		public static Span<T, Span<T, Span<T, Span<T, Span<T>>>>> Alloc32<T>() =>
+			Alloc24<T>().Add<T, Span<T, Span<T, Span<T, Span<T>>>>>();
+	}
+}
