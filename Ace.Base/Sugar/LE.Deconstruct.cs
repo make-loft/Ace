d1ca1552e0f63f4817c0575f1f476b2adb200425
makeman <makeman@tut.by>
2018-10-02 13:24:41 +0300

    MAKE !

--- Ace.Base/Sugar/LE.Deconstruct.cs
+++ Ace.Base/Sugar/LE.Deconstruct.cs
@@ -18,8 +18,8 @@ namespace Ace
 		public static T To<T>(this T o, out T a, out T b, out T c) => a = b = c = o;
 		public static T To<T>(this T o, out T a, out T b, out T c, out T d) => a = b = c = d = o;
 		public static T To<T>(this T o, out T a, out T b, out T c, out T d, out T e) => a = b = c = d = e = o;
-		public static T To<T>(this T o, out T a, out T b, out T c, out T d, out T e, out T f) => a =b = c = d = e = f = o;
-		public static T To<T>(this T o, out T a, out T b, out T c, out T d, out T e, out T f, out T g) => a =b = c = d = e = f = g = o;
-		public static T To<T>(this T o, out T a, out T b, out T c, out T d, out T e, out T f, out T g, out T h) => a =b = c = d = e = f = g = h = o;
+		public static T To<T>(this T o, out T a, out T b, out T c, out T d, out T e, out T f) => a = b = c = d = e = f = o;
+		public static T To<T>(this T o, out T a, out T b, out T c, out T d, out T e, out T f, out T g) => a = b = c = d = e = f = g = o;
+		public static T To<T>(this T o, out T a, out T b, out T c, out T d, out T e, out T f, out T g, out T h) => a = b = c = d = e = f = g = h = o;
 	}
 }
