a9f70c4eaef690b7b057d3c57b1a60d01fd0ab8b
makeman <makeman@tut.by>
2019-01-19 09:28:02 +0300

    MAKE !

--- Ace.Zest/Markup/Set.cs
+++ Ace.Zest/Markup/Set.cs
@@ -1,8 +1,14 @@
-﻿using System.Collections.ObjectModel;
+﻿using System.Collections;
+using System.Collections.ObjectModel;
+using System.Linq;
 
 namespace Ace.Markup
 {
 	public class Set : ObservableCollection<object>
 	{
+		public IList Source
+		{
+			set => this.MergeMany(value.Cast<object>());
+		}
 	}
 }
