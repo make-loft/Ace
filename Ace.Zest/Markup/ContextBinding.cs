0813ace058f17f3066c7c31393e79ba90669abbf
makeman <makeman@tut.by>
2019-02-12 15:18:27 +0300

    MAKE !

---
+++ Ace.Zest/Markup/ContextBinding.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Globalization;
+using Ace.Markup.Patterns;
+
+namespace Ace.Markup
+{
+	public class ContextBinding : ABindingExtension
+	{
+		public ContextBinding() : base(null) => Key = null;
+		public ContextBinding(string key) : base(null) => Key = key;
+
+		public string Key { get; set; }
+
+		public override object Convert(object value, Type targetType, object parameter, CultureInfo culture) =>
+			value.To<ContextObject>().GetMediator(Ace.Context.Get(Key));
+	}
+}
