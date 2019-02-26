4473083be62352ba63a88d19d89abad0a8853405
makeman <makeman@tut.by>
2019-02-27 02:38:53 +0300

    MAKE !

---
+++ Ace.Zest/Sugar/Invoke.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Ace
+{
+	public class Invoke
+	{
+		public static async Task Delay(int milliseconds, Action callback)
+		{
+			await Task.Delay(milliseconds);
+			callback?.Invoke();
+		}
+	}
+}
