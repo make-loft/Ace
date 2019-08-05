c927a80aa15a4d50c9de64bc41ecc377269aa79d
Makeloft <makeman@tut.by>
2019-08-05 11:46:13 +0300

    MAKE !

--- Ace.Base/Replication/Models/Map.cs
+++ Ace.Base/Replication/Models/Map.cs
@@ -1,12 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 
 namespace Ace.Replication.Models
 {
-	public class Map : Dictionary<string, object>
+	public class Map : Dictionary<string, object>, INotifyPropertyChanged
 	{
 		public Map() { }
 		public Map(IDictionary<string, object> dictionary) : base(dictionary) { }
+
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		public void EvokePropertyChanged(string propertyName = "Item[]") =>
+			PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
 
 		public new void Add(string key, object value)
 		{
