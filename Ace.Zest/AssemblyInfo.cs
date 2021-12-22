cd319470794a092d1bf180aa3ed7f3e368e24941
Makeloft <makeman@tut.by>
2021-12-22 23:46:03 +0300

    MAKE !

---
+++ Ace.Zest/AssemblyInfo.cs
@@ -0,0 +1,5 @@
+﻿using System.Windows.Markup;
+
+[assembly: XmlnsPrefix("http://xamarin.com/schemas/2014/forms", "xamarin")]
+[assembly: XmlnsDefinition("http://xamarin.com/schemas/2014/forms", "Ace.Markup")]
+[assembly: XmlnsDefinition("http://xamarin.com/schemas/2014/forms", "Ace.Converters")]
---
+++ Ace.Zest/Converters/TwoWayConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+#if XAMARIN
+using Xamarin.Forms;
+#else
+using System.Windows.Data;
+#endif
+
+namespace Ace.Converters
+{
+	public class TwoWayConverter : IValueConverter
+	{
+		public IValueConverter GetConverter { get; set; }
+		public IValueConverter SetConverter { get; set; }
+
+		object IValueConverter.Convert(object value, Type targetType, object parameter, CultureInfo culture) =>
+			GetConverter.Convert(value, targetType, parameter, culture);
+
+		object IValueConverter.ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) =>
+			SetConverter.Convert(value, targetType, parameter, culture);
+	}
+}
