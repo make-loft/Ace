3e9e5911be7c7ef283b7d3b9d85c84b653af700a
Makeloft <makeman@tut.by>
2022-05-31 15:28:07 +0300

    MAKE !

--- Ace.Zest/Markup/ValueExtensions.cs
+++ Ace.Zest/Markup/ValueExtensions.cs
@@ -40,4 +40,24 @@ namespace Ace.Markup
 		public static readonly object BoxedValue = true;
 		public override object Provide(object targetObject, object targetProperty) => BoxedValue;
 	}
+
+#if DESKTOP
+	public class VisibleExtension : AMarkupExtension
+	{
+		public static readonly object BoxedValue = System.Windows.Visibility.Visible;
+		public override object Provide(object targetObject, object targetProperty) => BoxedValue;
+	}
+
+	public class CollapsedExtension : AMarkupExtension
+	{
+		public static readonly object BoxedValue = System.Windows.Visibility.Collapsed;
+		public override object Provide(object targetObject, object targetProperty) => BoxedValue;
+	}
+
+	public class HiddenExtension : AMarkupExtension
+	{
+		public static readonly object BoxedValue = System.Windows.Visibility.Hidden;
+		public override object Provide(object targetObject, object targetProperty) => BoxedValue;
+	}
+#endif
 }
