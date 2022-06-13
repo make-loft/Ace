e47c5c30df2372076d35e4a9ba413fe5d4a0049a
Makeloft <makeman@tut.by>
2022-06-13 22:01:03 +0300

    MAKE !

---
+++ Ace.Base/Sugar/LE.Interpop.cs
@@ -0,0 +1,27 @@
+﻿#if DESKTOP
+
+using System.Runtime.InteropServices;
+
+namespace Ace
+{
+    static partial class LE
+    {
+		public static T To<T>(this byte[] bytes)
+		{
+			{
+				var handle = GCHandle.Alloc(bytes, GCHandleType.Pinned);
+
+				try
+				{
+					var structure = (T)Marshal.PtrToStructure(handle.AddrOfPinnedObject(), typeof(T));
+					return structure;
+				}
+				finally
+				{
+					handle.Free();
+				}
+			}
+		}
+	}
+}
+#endif
