6cdcb53ccbe060855b8825f1be8d00e707923bba
Makeloft <makeman@tut.by>
2022-08-15 15:56:24 +0300

    MAKE !

--- Ace.Core/Expression.cs
+++ Ace.Core/Expression.cs
@@ -3,14 +3,16 @@ using System.Linq.Expressions;
 
 namespace Ace
 {
-	public static class Expression
+	public static class Expression_
 	{
-		public static System.Linq.Expressions.Expression Unbox<TReturn>(this Expression<Func<TReturn>> expression) =>
-			expression.Body is UnaryExpression unaryExpression
+		public static string UnboxMemberName(this LambdaExpression lambdaExpression) =>
+			lambdaExpression.Body.Unbox() is MemberExpression memberExpression
+				? memberExpression.Member.Name
+				: throw new ArgumentException(lambdaExpression.ToString());
+
+		private static Expression Unbox(this Expression expression) =>
+			expression is UnaryExpression unaryExpression
 				? unaryExpression.Operand
-				: expression.Body;
-		
-		public static string UnboxMemberName<TReturn>(this Expression<Func<TReturn>> expression) =>
-			expression.Unbox().As<MemberExpression>()?.Member.Name ?? throw new ArgumentException(expression.ToString());
+				: expression;
 	}
 }
