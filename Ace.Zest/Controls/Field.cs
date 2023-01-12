1fab00f664b46a1f65afdf743cf5e097e9fc6fca
Makeloft <makeman@tut.by>
2023-01-12 19:57:26 +0300

    MAKE !

---
+++ Ace.Zest/Controls/Field.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading.Tasks;
+
+namespace Ace.Controls
+{
+	public class Field : Xamarin.Forms.Entry
+	{
+		public Field()
+		{
+			bool isCaptured = false;
+
+			TextChanged += async (o, e) =>
+			{
+				if (isCaptured || IsEnabled.Not())
+					return;
+
+				IsEnabled = false;
+				await Task.Delay(2000);
+				IsEnabled = true;
+			};
+
+			Focused += (o, e) => isCaptured = true;
+
+			Unfocused += async (o, e) =>
+			{
+				await Task.Delay(1000);
+				isCaptured = false;
+			};
+		}
+	}
+}
