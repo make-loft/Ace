d5e3f6d34ae6d7d30cb61c07b56acbb3bf4a3c92
Makeloft <makeman@tut.by>
2023-02-06 22:41:56 +0300

    MAKE !

--- Ace.Zest/Dictionaries/AppConverters.Xamarin.xaml.cs
+++ Ace.Zest/Dictionaries/AppConverters.Xamarin.xaml.cs
@@ -6,5 +6,7 @@ namespace Ace.Zest.Dictionaries
 	public partial class AppConverters
 	{
 		public AppConverters() => InitializeComponent();
+
+		private object Not_Convert(Markup.Patterns.ConvertArgs args) => args.Value.Is(out bool b) ? b.Not() : args.Value;
 	}
 }
--- Ace.Zest/Dictionaries/AppConverters.xaml.cs
+++ Ace.Zest/Dictionaries/AppConverters.xaml.cs
@@ -3,5 +3,7 @@
 	public partial class AppConverters
 	{
 		public AppConverters() => InitializeComponent();
+
+		private object Not_Convert(Markup.Patterns.ConvertArgs args) => args.Value.Is(out bool b) ? b.Not() : args.Value;
 	}
 }
