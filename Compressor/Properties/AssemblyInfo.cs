249f33ba4e1843165d9fa6cbedc0cecaef9c6e76
Makeman <makeman@tut.by>
2026-06-19 00:53:00 +0300

    MAKE !

---
+++ Compressor/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("Compressor")]
+[assembly: AssemblyProduct("Compressor")]
+[assembly: AssemblyCopyright("© Makeloft Studio")]
+[assembly: AssemblyVersion("0.0.0.0")]
